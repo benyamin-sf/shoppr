b32f1f53a661645b6688a818c0c9e7e84a5de60d
Benyamin Sajedi Fard <ben.8108.1611@gmail.com>
2025-07-28 13:49:15 +0330

    Updated: some minor issues in user model.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,11 +2,26 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: String,
-    lastName: String,
-    fullName: String,
-    email: { type: String, unique: true, required: true },
-    phoneNumber: { type: String, unique: true, required: true },
+    firstName: {
+      type: String,
+      required: [true, "'first name' is required."],
+      maxLength: [25, 'First name should be less than 25 characters.'],
+    },
+    lastName: {
+      type: String,
+      required: [true, "'last name' is required."],
+      maxLength: [50, 'Last name should be less than 50 characters.'],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "'email address' is required."],
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      required: [true, "'phone number' is required."],
+    },
     password: String,
     role: { type: String, enum: ['user', 'seller', 'admin'], default: 'user' },
     addresses: [AddressSchema],
@@ -16,5 +31,9 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.method('fullName', function () {
+  return `${this.firstName} ${this.lastName}`;
+});
 
 export default mongoose.model('user', userSchema);
